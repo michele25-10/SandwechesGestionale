384f6135bc4defcee4a70d8c193a244c1731fece
michele25-10 <michele.gabri01@gmail.com>
2023-01-10 16:06:17 +0100

    creazione endpoints.dart

---
+++ sandweches_admin/lib/utils/endpoints.dart
@@ -0,0 +1,12 @@
+// hostname
+const android = "192.168.235.36";
+const ios = "127.0.0.1";
+const vm = "192.168.0.230:8081";
+const raspberry = "192.168.31.151";
+
+const baseUrl = "$android/food-api";
+// const baseUrl = "$ios/food-api";
+// const baseUrl = "$raspberry/food-api";
+// const baseUrl = vm;
+
+// url
