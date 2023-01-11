154792590cb20cc0645a6b08c38d69c2a0bb7e67
michele25-10 <michele.gabri01@gmail.com>
2023-01-11 19:12:48 +0100

    Add pickup, funzione sistemata

--- uffici/function/addPickupPost.php
+++ uffici/function/addPickupPost.php
@@ -1,7 +1,27 @@
 <?php
 
 function addPickup($data){
+    $url = 'http://localhost/webApp_sandweches/food-api/API/order/pickup/addPickup.php';
+
+    $curl = curl_init($url);    //inizializza una nuova sessione di cUrl
+    //Curl contiene il return del curl_init 
+
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_POST, true);
     
+    $headers = array(
+        "Content-Type: application/json",
+        "Content-Lenght: 0",
+    );
+
+    curl_setopt($curl, CURLOPT_HTTPHEADER, $headers); // setta gli headers della request
+
+    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+
+    $responseJson = curl_exec($curl);   //eseguo
+
+    curl_close($curl);  //chiudo sessione
+
 }
 
 ?>
