122f802fcf0554c773bb4b983415fa4b355e5678
michele25-10 <michele.gabri01@gmail.com>
2023-01-13 19:13:33 +0100

    Disactive user funzionante

--- uffici/function/user.php
+++ uffici/function/user.php
@@ -5,11 +5,11 @@ function disactiveUser($data){
     $id = $data;
     echo $id;
 
-    $url = "http://localhost/webApp_sandweches/food-api/user/deleteAccount.php?id=" . $id; 
+    $url = "http://localhost/webApp_sandweches/food-api/API/user/deleteAccount.php?id=" . $id; 
 
     $json_data = file_get_contents($url);
 
-    echo $json_data; 
+    return $json_data; 
 }
 
 ?>
