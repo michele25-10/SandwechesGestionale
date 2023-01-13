2a524584e026f777cb9b0f18a697457f7d6ed716
Marco-Malanchin <72445677+Marco-Malanchin@users.noreply.github.com>
2023-01-13 11:08:51 +0100

    corretto

--- uffici/function/login.php
+++ uffici/function/login.php
@@ -3,7 +3,7 @@
 function login($data)
     {
        // $url = 'http://localhost:8080/Progetto-Panini/food-api/API/user/login.php';
-        $url = 'http://localhost/webApp_sandweches-main/food-api/API/user/login.php';
+        $url = 'http://localhost/webApp_sandweches/food-api/API/user/login.php';
 
         $curl = curl_init($url);    //inizializza una nuova sessione di cUrl
         //Curl contiene il return del curl_init 
