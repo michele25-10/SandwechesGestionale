ee0b3445d41db667aaf4e4de132ba8c33fed04f5
michele25-10 <michele.gabri01@gmail.com>
2023-01-14 10:59:50 +0100

    Sistemato getOrderByUser

--- uffici/function/getOrder.php
+++ uffici/function/getOrder.php
@@ -30,9 +30,24 @@ function getOrderUser($data){
     
     $json_data = file_get_contents($url);
 
-    echo $json_data; 
+    $decode_data = json_decode($json_data, $assoc=true);
+    $order_data = $decode_data;
+    $order_arr=array();
 
-    $decode_data = json_decode($json_data);
+    foreach ($order_data as $order) {
+      $order_record = array(
+        'id' => $order['id'],
+        'user' => $order['user'],
+        'created' => $order['created'], 
+        'pickup' => $order['pickup'], 
+        'break' => $order['break'],
+        'status' => $order['status'], 
+        'json' => $order['json'],
+        );
+        array_push($order_arr,$order_record);
+    }
+
+    return $order_arr;  
 }
 
 ?>
--- uffici/pages/getOrderByUser.php
+++ uffici/pages/getOrderByUser.php
@@ -7,7 +7,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   if (!empty($_POST['id'])) {
     $data = $_POST['id'];
 
-    getOrderUser($data);
+    $order_arr = getOrderUser($data);
 }
 }
  
@@ -32,3 +32,39 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     </div>
   </body>
 </html>
+
+<?php
+//session_start();
+
+include_once dirname(__FILE__) . '/../function/getOrder.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  if (!empty($_POST['id'])) {
+    $data = $_POST['id'];
+
+    $order_arr = getOrderUser($data);
+    
+    if(!empty($order_arr)){
+
+      echo('<table>');
+      echo('<tr>'); 
+      echo('<td>ID ordine</td><td>ID user</td><td>Creazione</td><td>Punto consegna ID</td><td>Intervallo id</td><td>Id status</td><td>json</td>'); 
+      echo('</tr>'); 
+        foreach($order_arr as $row) {
+            //ogni elemento dell'array è un array a sua volta, per la precisione una riga della tabella
+            echo('<tr>');
+            foreach($row as $cell) {
+                //ogni elemento della riga è finalmente una cella
+                echo('<td>'.$cell.'</td>');
+            }
+            echo("</tr>\n");
+        }
+        echo('</table>');
+  
+
+    }
+
+  }
+}
+ 
+?>
