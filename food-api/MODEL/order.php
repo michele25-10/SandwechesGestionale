92eab0e84fd4c2ca5e28d2277b324e4c61b5b466
michele25-10 <michele.gabri01@gmail.com>
2023-01-18 11:58:44 +0100

    modificata query

--- food-api/MODEL/order.php
+++ food-api/MODEL/order.php
@@ -35,8 +35,8 @@
             left join `user` u on u.id = o.`user` 
             left join break b on b.id  = o.break 
             left join pickup p on p.id = o.pickup 
-            left join status s ON s.id = o.id
-            where o.id = '1';";
+            left join status s ON s.id = o.status
+            where o.status = 1;";
 
             $stmt = $this->conn->query($query);
 
