8237d00efe9160930da3db733320606e4bc1cc64
Marco-Malanchin <72445677+Marco-Malanchin@users.noreply.github.com>
2023-01-18 13:01:44 +0100

    Update productQuantity.php
    work in progress

--- paninara/pages/productQuantity.php
+++ paninara/pages/productQuantity.php
@@ -53,12 +53,13 @@
   
         <div class="container">
             <div class="row mt-5">
+                <div class="col-6">
                 <h2>Modifca quantità prodotto</h2>
                 <div class="container-fluid">
-        <form method="post">
-            <input type="" id="name" placeholder="Quantità totale" name="quantity" maxlength="50" required>
-            <button type="submit" name="user">Invia</button>
-        </form>
+                    <form method="post">
+                        <input type="" id="name" placeholder="Quantità totale" name="quantity" maxlength="50" required>
+                        <button type="submit" name="user">Invia</button>
+                    </form>
         <?php
 //stringa di identificazione del server, quando premi button il metodo diventa post
 include_once dirname(__FILE__) . '/../function/product.php';
@@ -86,6 +87,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 ?>
             </div>
             </div>
+            <div class="col-6">
+            <h2>Modifca quantità prodotto</h2>
+                <div class="container-fluid">
+                    <form method="post">
+                        <input type="" id="name" placeholder="Quantità totale" name="quantity" maxlength="50" required>
+                        <button type="submit" name="user">Invia</button>
+                    </form>
+</div>
+            </div>
+
         </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
     </body>
