af8fb92e3fc8052d28e78bad9d6d864e3e83e1c3
Angelo Pavan <angelopavan.dev@gmail.com>
2023-01-18 13:21:05 +0100

    Aggiustata la homepage.

--- uffici/pages/homePage.php
+++ uffici/pages/homePage.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Sandwech | Admin</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-        <link rel="stylesheet" href="assets/style.css">
+        <link rel="stylesheet" href="../assets/style.css">
         <link rel="icon" type="image/x-icon" href="assets/img/logo.png">
     </head>
 
