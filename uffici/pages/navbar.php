d55c9d89d46d4521f01eb2a764d62bee24ba546c
Angelo Pavan <angelopavan.dev@gmail.com>
2023-01-20 22:24:33 +0100

    Fixata favicon.

--- uffici/pages/navbar.php
+++ uffici/pages/navbar.php
@@ -7,7 +7,7 @@
         <title>Sandwech | Login</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
         <link rel="stylesheet" href="assets/style.css">
-        <link rel="icon" type="image/x-icon" href="assets/img/logo.png">
+        <link rel="icon" type="image/x-icon" href="../assets/img/logo.png">
     </head>
     
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
