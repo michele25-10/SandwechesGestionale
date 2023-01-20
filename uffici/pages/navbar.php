e8c512d55879b8ed94c5ab6c14fcf98d14ea8351
Angelo Pavan <angelopavan.dev@gmail.com>
2023-01-20 22:11:58 +0100

    Creazione della pagina putProductQuantity.

--- uffici/pages/navbar.php
+++ uffici/pages/navbar.php
@@ -48,7 +48,7 @@
                         <li><a class="dropdown-item" href="addTag.php">Aggiungi Tags</a></li>
                         <li><a class="dropdown-item" href="">Cancella Tags</a></li>
                         <li><a class="dropdown-item" href="disactiveProduct.php">Disattiva Prodotti</a></li>
-                        <li><a class="dropdown-item" href="">Cambia Quantità</a></li>
+                        <li><a class="dropdown-item" href="putProductQuantity.php">Cambia Quantità</a></li>
                         <li><a class="dropdown-item" href="">Cambia Offerte</a></li>
                     </ul>
                 </li>
